c7776cc821a97899d196602b3923f9db0da7d988
0-BSCode <bryanipsanchez@gmail.com>
2024-10-05 11:08:02 +0800

    Log timestamp of request

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
 export const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    console.info(`[${req.method}] ${req.path}`);
+    console.info(`[${new Date().toISOString()}] ${req.method} ${req.path}`);
     next()
 }
