c38ee815d2ea824b6603bfbcdaf4d01e2e48bb11
0-BSCode <bryanipsanchez@gmail.com>
2024-10-05 10:19:04 +0800

    Refactor route handling

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,15 @@
 import { errorHandlerMiddleware } from "@middleware/error-handler.middleware";
 import express, { urlencoded, json } from "express";
-import userRoutes from "@routes/user.routes";
-import authRoutes from '@routes/auth.routes';
 import cookieParser from 'cookie-parser';
 import { envConfig } from '@config/env';
+import routes from "@routes/index";
 
 const app = express();
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
 app.use(cookieParser());
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
+app.use("/api", routes);
 
 // Error handler
 app.use(errorHandlerMiddleware);
