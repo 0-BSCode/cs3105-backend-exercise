204a1d472afe4e325ff93b2338ede02f49835eb0
0-BSCode <bryanipsanchez@gmail.com>
2024-10-05 12:07:20 +0800

    Add extra params to rate limiter

--- src/middleware/rate-limiter.middleware.ts
+++ src/middleware/rate-limiter.middleware.ts
@@ -3,6 +3,8 @@ import rateLimit from "express-rate-limit"
 export const rateLimiterMiddleware = rateLimit({
     windowMs: Number(process.env.RATE_LIMIT_WINDOW_MS),
     max: Number(process.env.RATE_LIMIT_MAX),
+    standardHeaders: true,
+    legacyHeaders: false,
     handler: (req, res) => {
         res.status(429).json({message: "Too many requests"})
     }
